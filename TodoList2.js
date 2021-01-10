ea2f7891e867c727c306377861a1e2cdf561b125
Shalini <37019489+Shivi169@users.noreply.github.com>
2021-01-11 01:21:17 +0530

    Update TodoList2.js

--- TodoList2.js
+++ TodoList2.js
@@ -1,15 +1,70 @@
+import React, { Component } from "react";
+import TodoItems from "./TodoItems";
+
 class TodoList extends Component {
-  render() {
-    return (
-      <div className="todoListMain">
-        <div className="header">
-          <form onSubmit={this.addItem}>
-            <input placeholder="enter task">
-            </input>
-            <button type="submit">add</button>
-          </form>
-        </div>
-      </div>
-    );
+   constructor(props) {
+    super(props);
+ 
+    this.addItem = this.addItem.bind(this);
   }
+  
+   addItem(e) {
+  if (this._inputElement.value !== "") {
+    var newItem = {
+      text: this._inputElement.value,
+      key: Date.now()
+    };
+ 
+    this.setState((prevState) => {
+      return { 
+        items: prevState.items.concat(newItem) 
+      };
+    });
+   
+    this._inputElement.value = "";
+  }
+   
+  console.log(this.state.items);
+     
+  e.preventDefault();
 }
+}
+  
+constructor(props) {
+  super(props);
+ 
+  this.state = {
+    items: []
+  };
+ 
+  this.addItem = this.addItem.bind(this);
+}
+  
+ render()  {
+  return (
+    <div className="todoListMain">
+      <div className="header">
+        <form onSubmit={this.addItem}>
+          <input ref={(a) => this._inputElement = a} 
+                  placeholder="enter task">
+          </input>
+          <button type="submit">add</button>
+        </form>
+      </div>
+      <TodoItems entries={this.state.items}/>
+    </div>
+  );
+}
+
+var newItem = {
+  text: this._inputElement.value,
+  key: Date.now()
+};
+
+this.setState((prevState) => {
+  return { 
+    items: prevState.items.concat(newItem) 
+  };
+});
+
+e.preventDefault();
